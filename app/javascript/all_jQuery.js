4cda70b71fd995e4089fcb41f96c6c5480535cc0
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-01-14 02:02:39 +0000

    [Update]UI

--- app/javascript/all_jQuery.js
+++ app/javascript/all_jQuery.js
@@ -2,4 +2,5 @@ $(function() {
   $('.jQuery_aplly').hide().fadeIn('slow');
   $('header').hide().fadeIn('slow');
   $('footer').hide().fadeIn('slow');
+  $('.jQuery_aplly_slide').hide().slideDown('slow');
 });
